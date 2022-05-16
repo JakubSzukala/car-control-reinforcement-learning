799c3073da1eb0bd04a9aa4cfdc470ea26452c8f
JakubSzukala <burski09@outlook.com>
2022-05-16 11:13:35 +0200

    initial setup

---
+++ main.py
@@ -0,0 +1,24 @@
+import gym
+import matplotlib
+import matplotlib.pyplot as plt
+
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+env = gym.make('CarRacing-v1')
+#env = gym.make('LunarLander-v2')
+env.action_space.seed(42)
+
+observation, info = env.reset(seed=42, return_info=True)
+
+for _ in range(1000):
+    env.render()
+    observation, reward, done, info = env.step(env.action_space.sample())
+    #print('Observation: ', observation)
+    print('Random action: ', env.action_space.sample())
+    print('Reward: ', reward)
+    if done:
+        observation, info = env.reset(return_info=True)
+
+env.close()
---
+++ model.py
@@ -0,0 +1,11 @@
+import random
+import math
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as T
